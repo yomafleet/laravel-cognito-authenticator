b46d54cb2c3afc6cb1202e69dbfd9ed0f9691cdc
aungchitminn <99715319+aungchitminn@users.noreply.github.com>
2023-03-13 19:47:09 +0630

    fix: update region env key

--- config/cognito.php
+++ config/cognito.php
@@ -2,7 +2,7 @@
 
 return [
     'pool_id' => env('AWS_COGNITO_USER_POOL_ID'),
-    'region' => env('AWS_REGION'),
+    'region' => env('AWS_DEFAULT_REGION'),
     'id_token_name' => env('AWS_COGNITO_ID_TOKEN_NAME', 'X-ID-Token'),
     'client_ids' => env('AWS_COGNITO_CLIENT_IDS', ''),
 ];
