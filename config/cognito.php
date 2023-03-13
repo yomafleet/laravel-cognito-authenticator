88f34ac3faa4c0b581c529ec37d5b26d27ded71c
aungchitminn <99715319+aungchitminn@users.noreply.github.com>
2023-03-13 19:20:14 +0630

    fix: request header name malformed

--- config/cognito.php
+++ config/cognito.php
@@ -3,6 +3,6 @@
 return [
     'pool_id' => env('AWS_COGNITO_USER_POOL_ID'),
     'region' => env('AWS_REGION'),
-    'id_token_name' => env('AWS_COGNITO_ID_TOKEN_NAME', 'X_ID_Token'),
+    'id_token_name' => env('AWS_COGNITO_ID_TOKEN_NAME', 'X-ID-Token'),
     'client_ids' => env('AWS_COGNITO_CLIENT_IDS', ''),
 ];
