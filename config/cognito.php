1703fb5fa8064a0e9c4299c15dfebd8df15b116e
aungchitminn <99715319+aungchitminn@users.noreply.github.com>
2023-03-15 00:49:28 +0630

    feat: update env

--- config/cognito.php
+++ config/cognito.php
@@ -6,7 +6,7 @@ return [
         'secret' => env('AWS_SECRET_ACCESS_KEY', ''),
     ],
     'pool_id' => env('AWS_COGNITO_USER_POOL_ID'),
-    'region' => env('AWS_DEFAULT_REGION'),
+    'region' => env('AWS_COGNITO_REGION'),
     'id_token_name' => env('AWS_COGNITO_ID_TOKEN_NAME', 'X-ID-Token'),
     'client_ids' => env('AWS_COGNITO_CLIENT_IDS', ''),
     'id' => env('AWS_COGNITO_CLIENT_ID'),
