852e1c8ab874619eae645f759f6e27411559eda7
aungchitminn <99715319+aungchitminn@users.noreply.github.com>
2023-03-15 08:59:20 +0630

    fix: add doc block

--- src/Facades/Cognito.php
+++ src/Facades/Cognito.php
@@ -7,6 +7,8 @@ use Illuminate\Support\Facades\Facade;
 /**
  * @method static array createVerifier(array $clientIds)
  * @method array authenticate($identifier, $password)
+ * @method \Yomafleet\CognitoAuthenticator\Contracts\TokenContract decode($token)
+ * @method \Yomafleet\CognitoAuthenticator\Contracts\ClaimVerifierContract createVerifier(array $clientIds)
  * @method static \Yomafleet\CognitoAuthenticator\JwtDecoder createJwtDecoder()
  * @method static \Yomafleet\CognitoAuthenticator\CognitoSubRetriever getSubRetriever(\Illuminate\Http\Request $request, \Yomafleet\CognitoAuthenticator\Contracts\DecoderContract|null $decoder)
  *
